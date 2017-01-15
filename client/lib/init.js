6717eda99fa1266b2d30529eca76b1a5c2c12743
Tomohiro Matsuo <gacktomo@gmail.com>
2017-01-15 21:17:16 +0900

    add HTMLcontent View

--- client/lib/init.js
+++ client/lib/init.js
@@ -1,5 +1,8 @@
 //Web3インスタンスの生成
 web3 = new Web3();
+addr = "0xefbaa5228e7c7a2e91c6664e755433998701bf0a";
+abi = '[{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"pages","outputs":[{"name":"owner","type":"address"},{"name":"data","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"name","type":"string"}],"name":"get","outputs":[{"name":"data","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"source","type":"string"}],"name":"stringToBytes32","outputs":[{"name":"result","type":"bytes32"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"name","type":"string"},{"name":"data","type":"string"}],"name":"set","outputs":[],"payable":false,"type":"function"}]';
+homepage = "index";
 
 //RPCプロバイダを設定
 //URLの部分は読者の環境に合わせてください。（localhostの部分はIPアドレスにて指定してもかまいません。）
--- client/main.js
+++ client/main.js
@@ -2,28 +2,16 @@
 //web3オブジェクトのプロパティを取得する各種メソッドを定義。
 Template.nodeStatusComponent.helpers({
 
-  //接続先ノードの取得
-  currentProvider: function(){
-    return web3.currentProvider.host;
+  //ページ名の取得
+  pagename: function(){
+    return homepage;
   },
 
-  //接続先ノードのマイニング状態の取得
-  //マイニング中であればtrue、そうでなければfalse
-  isMining: function(){
-    return web3.eth.mining;
-  },
-
-  //接続先ノードのマイニングのハッシュレートを取得
-  currentHashrate: function(){
-    return web3.eth.hashrate;
-  },
-
-  //接続先ノードのピア数の取得
+  //コンテンツの取得
   htmlContent: function(){
-    var addr = "0x79e61820d8c75648ee17a38c43ae8c17d46a5369";
-    var abi = '[{"constant":true,"inputs":[],"name":"get","outputs":[{"name":"data","type":"string"}],"payable":false,"type":"function"},{"inputs":[{"name":"data","type":"string"}],"payable":false,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"owner","type":"address"},{"indexed":false,"name":"data","type":"string"}],"name":"RegisterEvent","type":"event"}]';
     var abi_json = JSON.parse(abi);
     var cnt = web3.eth.contract(abi_json).at(addr);
-    return cnt.get();
+    return cnt.get(homepage);
+    //return "<h1>title</h1><p>test</p>";
   }
 });
